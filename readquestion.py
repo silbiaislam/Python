1131d45e03ef0bf5f70ea895e240b2f466b53c37
Silbia Islam <43888978+silbiaislam@users.noreply.github.com>
2021-10-10 20:49:30 +0530

    Update readquestion.py

--- readquestion.py
+++ readquestion.py
@@ -1,9 +1,14 @@
 f = open("data.txt", "rt")
 lines = f.readlines()
-
-for line in lines:
-    if any(question_word in line for question_word in ['how', 'what', 'where']):
-        print(line.strip('\n'), "Yes") # remove new-lines to match desired output
-    else:
-        print(line.strip('\n'), "No")
 f.close()
+
+with open('data_out.txt', 'w') as f:
+    for line in lines:
+        if any(question_word in line for question_word in ['how', 'what', 'where']):
+            output = '{} {}\n'.format(line.strip('\n'), "Yes")
+            print(output)
+            f.write(output)
+        else:
+            output = '{} {}\n'.format(line.strip('\n'), "No")
+            print(output)
+            f.write(output)
